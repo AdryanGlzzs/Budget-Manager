6c790b23295b500e2a2769ca9a6a0cb3bb85fc37
Adryan Gonçalves <adryanglzzs@gmail.com>
2026-07-22 15:57:24 -0300

    feat: initialize backend project with Express server and registration route

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 import { Request, Response } from 'express'
 import { routes } from './routes/routes'
+import cors from 'cors'
 
 const app = express()
-app.use(express.json)
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+app.use(express.json())
 app.use(routes)
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "API funcionando!"
-  });
-});
+
 
 const PORT = 3000;
 
--- Back/src/routes/routes.ts
+++ Back/src/routes/routes.ts
@@ -1,4 +1,17 @@
 import express from 'express'
 import { Router } from 'express'
+import { Request, Response } from 'express'
 
-export const routes = Router()
+export const routes = Router()  
+
+
+
+routes.post('/register', (req: Request, res: Response) =>{
+    const {name, email, password, confirmPassword} = req.body
+    console.log(req.body)
+
+    return res.status(201).json({
+        message: "Usuário cadastrado com sucesso!",
+        user: { name, email, password, confirmPassword }
+    })
+})
